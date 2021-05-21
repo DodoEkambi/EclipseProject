02895c3f345d0ae28d0ea4c5465924daf3e71bc0
DodoEkambi <gkhngul2@gmail.com>
2021-05-22 00:27:19 +0300

    Class Variable(sınıf değişkeni) değişikliği

--- DenemeProjesi/src/Test.java
+++ DenemeProjesi/src/Test.java
@@ -1,13 +1,17 @@
 
 public class Test {
+	private String name = "nameless cat";
+	
 
+	public void setName(String name) {
+        //write your code here
+        this.name=name;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloo Dodoo");
-		System.out.println("Dodo Hellooo");
-		System.out.println("This is the third line");
-		System.out.println("This is the fourth line");
-		System.out.println("This is the fifth line");
+		Test cat = new Test();
+		cat.setName("Simba");
+		System.out.println(cat.name);
 	}
 
 }
